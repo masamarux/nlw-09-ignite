091537cbe985dda3dd95376259b084105191ff85
Marcelo Alves <alves-marcelo97@hotmail.com>
2022-09-16 20:07:44 -0300

    stage 02

---
+++ README.md
@@ -0,0 +1,2 @@
+# nlw-09-ignite
+NLW Esports Trilha Ignite
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(({ command, mode }) => {
+  //adicionei essa env para abrir um navegador diferente do meu principal, só adicionar um arquivo .env com o um parametro BROWSER com o nome do seu navegador
+  process.env.BROWSER=loadEnv(mode, process.cwd(), '').BROWSER;
+
+  return {
+    plugins: [react()],
+    server: {
+      open: true,
+    },
+  }
 })
